ca44acc7356795544cd55d8f15a7d7f40dacf141
moskib <30794120+moskib@users.noreply.github.com>
2018-08-24 18:31:39 +0300

    added fundamental components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule
